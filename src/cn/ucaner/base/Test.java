8528561336f7496db910f78626bcc65e73ff7816
Gooogle <jasonandy@hotmail.com>
2018-03-09 15:31:58 +0800

    :tada:

---
+++ src/cn/ucaner/base/Test.java
@@ -0,0 +1,29 @@
+/**
+ * <html>
+ * <body>
+ *  <P> Copyright 1994-2018. JasonInternational.</p>
+ *  <p> All rights reserved.</p>
+ *  <p> Created on 2018年3月9日 下午3:26:55</p>
+ *  <p> Created by Jason</p>
+ *  </body>
+ * </html>
+ */
+package cn.ucaner.base;
+
+/**     
+ * @Package：cn.ucaner.base   
+ * @ClassName：Test   
+ * @Description：   <p> Test </p>
+ * @Author： - Jason   
+ * @CreatTime：2018年3月9日 下午3:26:55   
+ * @Modify By：   
+ * @ModifyTime：  2018年3月9日
+ * @Modify marker：   
+ * @version    V1.0
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+}
