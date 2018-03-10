71321a2d47768b4e69f0ebee49040af57c647c6b
Gooogle <jasonandy@hotmail.com>
2018-03-10 17:09:38 +0800

    :tada:

---
+++ src/cn/ucaner/classreflect/Test.java
@@ -0,0 +1,31 @@
+/**
+ * <html>
+ * <body>
+ *  <P> Copyright 1994-2018 JasonInternation.</p>
+ *  <p> All rights reserved.</p>
+ *  <p> Created on 2018年3月10日</p>
+ *  <p> Created by Jason</p>
+ *  </body>
+ * </html>
+ */
+package cn.ucaner.classreflect;
+
+/**     
+ * @Package：cn.ucaner.classreflect   
+ * @ClassName：Test   
+ * @Description：   <p> Jason</p>
+ * @Author： - Jason   
+ * @CreatTime：2018年3月10日 下午5:06:37   
+ * @Modify By：   
+ * @ModifyTime：  2018年3月10日
+ * @Modify marker：   
+ * @version    V1.0
+ */
+public class Test {
+	
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+
+}
